e308840b8860ff31778d09404702916da4fd17fc
UliBaker <ulibaker@MacBook-Pro-de-UliBaker.local>
2024-11-02 12:44:42 -0600

    03-mouse-follower-spirit-bomb

--- projects/03-mouse-follower/src/App.jsx
+++ projects/03-mouse-follower/src/App.jsx
@@ -1,33 +1,72 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react"
+
+const FollowMouse = () => {
+  const [enabled, setEnabled] = useState(false)
+  const [position, setPosition] = useState({ x: 0, y: 0 })
+
+  useEffect(() => {
+    //console.log('effect ', { enabled })
+
+    const handleMove = (event) => {
+      const { clientX, clientY } = event
+      //console.log('handleMove', { clientX, clientY })  
+      setPosition({ x: clientX, y: clientY })
+    }
+
+    if(enabled) {
+      window.addEventListener('pointermove', handleMove)
+    }
+
+    return () => {
+      console.log('turned off')
+      window.removeEventListener('pointermove', handleMove)
+    }
+  }, [enabled])
+  return (
+    <>
+      <div style={{
+        position: 'absolute',
+        background: 'rgba(0, 0, 0, 0)',
+        border: '1px dotted rgba(0,255,237,1)',
+        borderRadius: '50%',
+        pointerEvents: 'none',
+        left: -52,
+        top: -52,
+        width: 102,
+        height: 102,
+        transform: `translate(${position.x}px, ${position.y}px)`
+      }}/>
+      <div style={{
+        position: 'absolute',
+        background: 'radial-gradient(circle, rgba(255,255,255,0.75) 50%, rgba(0,255,237,0.75) 75%, rgba(0,0,0,0.75) 100%)',
+        borderRadius: '50%',
+        pointerEvents: 'none',
+        left: -50,
+        top: -50,
+        width: 100,
+        height: 100,
+        transform: `translate(${position.x}px, ${position.y}px)`
+      }}/>
+      <button onClick={() => setEnabled(!enabled)}>
+        { enabled ? 'Turn off' : 'Turn on'}
+      </button>
+    </>
+  )
+}
 
 function App() {
-  const [count, setCount] = useState(0)
+const [mounted, setMounted] = useState(true)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Projecto 3: Spirit Bomb</h1>
+      { mounted ? <FollowMouse/> : 
+      <button>
+        Unavailable
+      </button> }
+      <button onClick={() => setMounted(!mounted)}>
+        { mounted ? 'Disappear' : 'Appear' }
+      </button>
     </>
   )
 }
